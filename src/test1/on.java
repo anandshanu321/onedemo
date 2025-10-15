f1c67803938ec51c6d1641d31e4f679856b510ee
Shanu Anand <shanuanand411@gmail.com>
2025-10-16 00:45:18 +0530

    second

---
+++ src/test1/on.java
@@ -0,0 +1,11 @@
+package test1;
+
+public class on {
+
+	public static void main(String[] args) {
+		System.out.println("sobhit");
+		System.out.println("sobhit");
+		System.out.println("sobhit");
+		System.out.println("sobhit");
+	}
+	}
