3b6c0a731d2b985badcb56d74fe00633859694a8
Shanu Anand <shanuanand411@gmail.com>
2025-10-16 00:25:34 +0530

    second

--- src/test1/tw.java
+++ src/test1/tw.java
@@ -3,6 +3,9 @@ package test1;
 public class tw {
 
 	public static void main(String[] args) {
+    System.out.println("amit");
+    System.out.println("shanu");
+    System.out.println("shanu");
     System.out.println("shanu");
 	}
 
